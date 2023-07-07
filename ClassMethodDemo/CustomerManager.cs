037f668314c1bf632872ab844f117b0066383918
Kübra Orhan <kubraorhanb@gmail.com>
2023-07-07 16:28:37 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    internal class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Name + "added");
+        }
+
+        public void listCustomer(Customer customer)
+        {
+            Console.WriteLine("Id:"+customer.Id);
+            Console.WriteLine("Name:" + customer.Name);
+            Console.WriteLine("Address:" + customer.Address);
+            Console.WriteLine("-----");
+
+        }
+
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMethodDemo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Kübra";
+            customer1.Address = "Bursa";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Necati";
+            customer2.Address = "Manisa";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Merve";
+            customer3.Address = "Muş";
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.Name = "Can";
+            customer4.Address = "Malatya";
+
+
+            Customer[] customers = new Customer[] {customer1,customer2,customer3,customer4 };
+
+            CustomerManager customerManager = new CustomerManager();    
+
+
+            for (int i = 0; i < customers.Length; i++)
+            {
+                customerManager.AddCustomer(customers[i]);
+                customerManager.listCustomer(customers[i]);
+            }
+        }
+    }
+}
